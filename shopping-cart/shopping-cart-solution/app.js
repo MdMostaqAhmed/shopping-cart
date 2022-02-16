34f00bfdd98bdf2baa4c95dc965ddf865e1c5013
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2022-02-16 07:13:19 +0600

    sub total cost, tax and grand total

--- shopping-cart/shopping-cart-solution/app.js
+++ shopping-cart/shopping-cart-solution/app.js
@@ -12,7 +12,26 @@ function updateProductNumber(product,price, isIncreasing) {
     //update total
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = productNumber * price;
+    // calculate total
+    calculateTotal();
 };
+
+function getInputValue(product){
+    const productNumber = parseInt(document.getElementById(product+'-number').value);
+    return productNumber;
+}
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax =  subTotal / 10;
+    const totalPrice = subTotal + tax;
+    //update om the html
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
+};
+
 //phone increase decrease event
 document.getElementById('phone-plus').addEventListener('click', function(){
     updateProductNumber("phone", 1219, true)
